065f98b6ddf37f245a7705e3b09a4a0266ad3699
BrantBart <brant.barton64@gmail.com>
2024-06-04 11:52:46 -0600

    linked lists

--- week03/teach/UniqueLetters.cs
+++ week03/teach/UniqueLetters.cs
@@ -10,9 +10,9 @@
         Console.WriteLine(AreUniqueLetters(test3)); // Expect True because its an empty string
     }
 
-    /// <summary>Determine if there are any duplicate letters in the text provided</summary>
-    /// <param name="text">Text to check for duplicate letters</param>
-    /// <returns>true if all letters are unique, otherwise false</returns>
+    // / <summary>Determine if there are any duplicate letters in the text provided</summary>
+    // / <param name="text">Text to check for duplicate letters</param>
+    // / <returns>true if all letters are unique, otherwise false</returns>
     private static bool AreUniqueLetters(string text) {
         // TODO Problem 1 - Replace the O(n^2) algorithm to use sets and O(n) efficiency
         for (var i = 0; i < text.Length; ++i) {
